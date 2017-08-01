8a94458ef165270392473baf42287dd59cb40b22
abbas <abbasyounis42244@gmail.com>
2017-08-02 01:58:36 +0300

    simple chat2

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/hello', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+app.get('/', function(req, res){
+  res.sendFile('hello world');
 });
 
 
