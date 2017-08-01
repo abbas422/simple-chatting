a43461fc40578cadfd91338194c760ab9b837e5e
abbas <abbasyounis42244@gmail.com>
2017-08-02 02:10:54 +0300

    simple chat5

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/hello', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/hello', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
 app.get('/', function(req, res){
   res.send('hello world');
 });
 
 
 
-io.on('connection', function(socket){
+// io.on('connection', function(socket){
 	
   
 
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg);
+//   });
+//});
 http.listen(process.env.PORT || 3100, function(){
   console.log('listening on *:3000');
 });
